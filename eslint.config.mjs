949f129b79aaec5db287f7c316003b36439e0625
terra <mordeworti@gmail.com>
2026-04-08 06:59:40 -0500

    refactor: eliminar eslint.config.mjs, usar solo TypeScript puro

--- eslint.config.mjs
+++
@@ -1,7 +0,0 @@
-import { register } from 'tsx/esm/api';
-register();
-
-// @ts-expect-error - ESM dynamic import
-const config = await import('./eslint.config.ts');
-
-export default config.default;
